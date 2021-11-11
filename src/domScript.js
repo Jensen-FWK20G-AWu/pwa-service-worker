eaa615dd6ee2cdcdadb0e885a8b3534354eb7d96
David Andersson <david.andersson@zocom.se>
2021-11-11 14:29:00 +0100

    Render list of animal facts

--- src/domScript.js
+++ src/domScript.js
@@ -2,6 +2,59 @@ console.log('Jag körs när DOM har laddats i webbläsaren');
 
 const sendRequestButton = document.querySelector('#sendRequestButton')
 const resultParagraph = document.querySelector('#resultParagraph')
+const factList = document.querySelector('#factList')
+const animalImg = document.querySelector('#animalImg')
+
+const state = {
+	facts: [
+		{
+			title: 'Gorillas can catch human colds and other illnesses.',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Gorille_des_plaines_de_l%27ouest_%C3%A0_l%27Espace_Zoologique.jpg/800px-Gorille_des_plaines_de_l%27ouest_%C3%A0_l%27Espace_Zoologique.jpg',
+			attribution: 'By Thurundir - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=84810598'
+		},
+		{
+			title: 'A newborn Chinese water deer is so small it can almost be held in the palm of the hand.',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hydropotes_inermis_male.JPG/1024px-Hydropotes_inermis_male.JPG',
+			attribution: 'By Altaileopard - Own work, Public Domain, https://commons.wikimedia.org/w/index.php?curid=15811909'
+		},
+		{
+			title: 'Ostriches can run faster than horses, and the males can roar like lions.',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Struthio_Diversity.jpg/1920px-Struthio_Diversity.jpg',
+			attribution: 'By PaleoNeolitic (montage creator)Diego Delsoninara - Wikimedia Commons, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=102322166'
+		},
+		{
+			title: 'A lion in the wild usually makes no more than twenty kills a year.',
+			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/1024px-Lion_waiting_in_Namibia.jpg',
+			attribution: 'By Kevin Pluck - Flickr: The King., CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=755560'
+		},
+	]
+}
+
+
+renderFacts()
+
+
+function renderFacts() {
+	state.facts.forEach(fact => {
+		let container = document.createElement('div')
+		let h3 = document.createElement('h3')
+		let button = document.createElement('button')
+		let p = document.createElement('p')
+
+		h3.innerText = fact.title
+		button.innerText = 'Show image'
+		p.innerText = fact.attribution
+
+		button.addEventListener('click', () => {
+			animalImg.src = fact.img
+		})
+
+		container.appendChild(h3)
+		container.appendChild(button)
+		container.appendChild(p)
+		factList.appendChild(container)
+	})
+}
 
 sendRequestButton.addEventListener('click', async () => {
 	let text = ''
