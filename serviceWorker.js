ffdf3fb4fcbbfb3afb9c19c3e37e858dd692836d
David Andersson <david.andersson@zocom.se>
2021-11-11 14:55:49 +0100

    SW adds files to cache when they are fetched

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,8 +1,15 @@
+const CACHE_NAME = 'v1'
 
 self.addEventListener('install', event => {
 	// Install-händelsen triggas när vi registrerar en NY service worker-fil (eller ändrad)
 	// Här bör vi konfigurera cache
 	console.log('SW installed at: ', new Date().toLocaleTimeString());
+
+	// Dessa filer ska alltid cachas, oberoende av om användaren behöver dem eller inte
+	const defaultFiles = ['https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Gorille_des_plaines_de_l%27ouest_%C3%A0_l%27Espace_Zoologique.jpg/800px-Gorille_des_plaines_de_l%27ouest_%C3%A0_l%27Espace_Zoologique.jpg', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hydropotes_inermis_male.JPG/1024px-Hydropotes_inermis_male.JPG']
+	event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+		return cache.addAll(defaultFiles)
+	}))
  });
  
 self.addEventListener('activate', event => {
@@ -15,17 +22,30 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
 	console.log('Fångade ett request: ', event.request.url);
 
-	if( !navigator.onLine ) {
-		// Offline-läge, svara på alla request med cachad resurs om den finns
-		event.respondWith(new Response('<h1>Sorry, you are offline</h1> <p>Wait for while and try again </p>', 
-			{ headers: { 'Content-Type': 'text/html' } } ));
-		return;
-	}
-	
+	// API gets special treatment
 	if( event.request.url.includes('simple.php') ) {
 		event.respondWith(new Response('<h1>This API is not allowed anymore</h1> <p>Try something else! </p>', 
 			{ headers: { 'Content-Type': 'text/html' } } ));
+		return
+	}
+
+	// Om det inte är ett API request - använda cache
+	if( navigator.onLine ) {
+		event.respondWith(fetch(event.request).then(response => {
+			let clone = response.clone()
+			caches.open(CACHE_NAME).then(cache => {
+				cache.put(event.request, clone)
+			})
+			return response
+		}))
+		
 	} else {
-		event.respondWith( fetch(event.request) )
+		// Kolla om det som vi ska hämta finns i webbläsarens cache
+		event.respondWith(caches.match(event.request).then(response => {
+			if( response ) {
+				return response
+			}
+			return null  // kanske bättre att returnera en standardbild?
+		}))
 	}
 })
